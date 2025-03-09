329049b7cfd5b5a646ec1617de02f352642b5035
Armia hanna <armiahanna089@gmail.com>
2025-03-09 22:13:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,44 +13,46 @@ let currentIndex = 0;
 let rightAnswer = 0;
 let countTime;
 function getJson() {
-    fetch("app.json")
-  .then(response => response.json()) 
-  .then(object => {
-      let ser = object.length;
-      // counter question
-      count(ser);
-      // question and counter
-      addQuestion(object[currentIndex], ser);
-      // time
-      counterTime(10, ser);
-      
-      // click button
-      btn.onclick = function () {
-          // to know right choice as I want it in JSON 
-          let rightAnswers = object[currentIndex]["right-answer"];
-          currentIndex++;
-          choose(rightAnswers, ser);
-          
-          // remove old question and add new question
-          questionArea.innerHTML = "";
-          answerArea.innerHTML = "";
-          
-          // add new question
-          addQuestion(object[currentIndex], ser);
-          
-          // handle span
-          handleSpan();
-          
-          // show result
-          resultShow(ser);
-          
-          // time
-          clearInterval(countTime);
-          counterTime(10, ser);
-      }
-  })
-  .catch(error => console.error("خطأ في تحميل الملف:", error));
+     var jsn = new XMLHttpRequest();
+    jsn.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let object = JSON.parse(this.responseText);
+            let ser = object.length;
+            //counter question
+            count(ser);
+            //question and counter
+            addQuestion(object[currentIndex], ser);
+            //time
+            counterTime(10, ser);
+            //click button
+            btn.onclick = function () {
+                // to know right choose as i want it in json 
+                let rightAnswers = object[currentIndex]["right-answer"];
+                currentIndex++;
+                choose(rightAnswers, ser);
+                // end question
+              
+                     //to remove question old and add new question
+               questionArea.innerHTML = "";
+                answerArea.innerHTML = "";
+                //to add new question
+                addQuestion(object[currentIndex], ser);
+                //handle span
+                handleSpan();
+                //show result
+                resultShow(ser); 
+                //time
+                clearInterval(countTime);
+            counterTime(10, ser);
 
+            }
+
+// console.log(object); 
+// console.log(ser)
+        }
+    };
+    jsn.open("GET", "http://localhost:5500/from/app.json", true);
+    jsn.send();
 }
 
 getJson();
@@ -177,4 +179,4 @@ function counterTime(time, ser) {
             
         },1000)
     }
-    }
+}
